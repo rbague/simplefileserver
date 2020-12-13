4d91be9d5fe6411d9123b15066bf0790423e98fb
Roger Bagué <rogerbague@gmail.com>
2020-12-13 18:51:34 +0100

    add https mode

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"path/filepath"
 
 	flag "github.com/spf13/pflag"
 )
@@ -13,6 +12,8 @@ func main() {
 	help := flag.Bool("help", false, "prints this help")
 	dir := flag.StringP("directory", "d", ".", "the directory of which to serve files")
 	port := flag.StringP("port", "p", "8080", "on which port to listen to")
+	cert := flag.String("cert-file", "", "path to the SSL certificate")
+	key := flag.String("key-file", "", "path to the SSL certificate's private key")
 	flag.Parse()
 
 	if *help {
@@ -20,15 +21,21 @@ func main() {
 		os.Exit(0)
 	}
 
-	directory, err := filepath.Abs(*dir)
-	if err != nil {
-		log.Fatalf("could not get absolute path from %q: %v", *dir, err)
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(*dir)))
+
+	var err error
+	if *cert != "" || *key != "" {
+		if *cert == "" || *key == "" {
+			log.Fatal("both 'cert-file' and 'key-file' flags must present to enable HTTPS")
+		}
+
+		err = http.ListenAndServeTLS(":"+*port, *cert, *key, mux)
+	} else {
+		err = http.ListenAndServe(":"+*port, mux)
 	}
 
-	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(directory)))
-
-	if err := http.ListenAndServe(":"+*port, mux); err != http.ErrServerClosed {
+	if err != nil && err != http.ErrServerClosed {
 		log.Fatalf("server closed unexpectedly: %v", err)
 	}
 }
